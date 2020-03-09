bd88099006279010bb4ab45801e10a81007f7c8d
agustin <agustin@agustin-HP-Laptop-15-bw0xx>
2020-03-09 16:24:45 -0300

    Commit Scanner DNI

--- public_html/js/controllers.js
+++ public_html/js/controllers.js
@@ -46,7 +46,7 @@ angular.module('app.controllers', [])
                                     prompt: "Por favor acerque el DNI a la camara para realizar el escaneo", // Android
                                     resultDisplayDuration: 500, // Android, display scanned text for X ms. 0 suppresses it entirely, default 1500
                                     formats: "QR_CODE,PDF_417", // default: all but PDF_417 and RSS_EXPANDED
-                                    orientation: "portrait", // Android only (portrait|landscape), default unset so it rotates with the device
+                                    orientation: "landscape", // Android only (portrait|landscape), default unset so it rotates with the device
                                     disableAnimations: true, // iOS
                                     disableSuccessBeep: false // iOS and Android
                                 }
